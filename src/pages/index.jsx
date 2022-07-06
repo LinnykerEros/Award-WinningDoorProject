6eb9d8905d6822867e415759ad8cddbaf00d11e4
Eros Linnyker <linnykereros@gmail.com>
2022-07-06 15:06:25 -0300

    Adicionando eventos as Portas

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,28 @@
 import { useState } from "react";
 import Porta from "../components/Porta";
 import PortaModel from "../model/porta";
-
+import criarPortas, { atualizarPortas } from "../functions/portas";
 export default function Home() {
-  const [p1, setP1] = useState(new PortaModel(1));
+  const [portas, setPortas] = useState(criarPortas(3, 2));
+
+  function renderizarPortas() {
+    return portas.map((porta) => {
+      return (
+        <Porta
+          key={porta.numero}
+          value={porta}
+          onChange={(novaPorta) =>
+            setPortas(atualizarPortas(portas, novaPorta))
+          }
+        />
+      );
+    });
+  }
 
   return (
     <div style={{ display: "flex" }}>
-      <Porta porta={p1} />
+      {/* onChange, chama uma nova porta, que me da a liberdade de alternar os estados */}
+      {renderizarPortas()}
     </div>
   );
 }
