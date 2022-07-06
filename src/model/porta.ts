be30a212ade37ddaeadf80e2fa22dbac7b860810
Eros Linnyker <linnykereros@gmail.com>
2022-07-06 11:56:00 -0300

    Integrando TypeScript

---
+++ src/model/porta.ts
@@ -0,0 +1,62 @@
+export default class PortaModel {
+  #numero: number;
+  #temPresente: boolean;
+  #selecionada: boolean;
+  #aberta: boolean;
+
+  constructor(
+    numero: number,
+    temPresente = false,
+    selecionada = false,
+    aberta = false
+  ) {
+    this.#numero = numero;
+    this.#temPresente = temPresente;
+    this.#selecionada = selecionada;
+    this.#aberta = aberta;
+  }
+
+  // metados para pegar os atributos
+  get numero() {
+    return this.#numero;
+  }
+  get temPresente() {
+    return this.#temPresente;
+  }
+  get selecionada() {
+    return this.#selecionada;
+  }
+  get aberta() {
+    return this.#aberta;
+  }
+
+  //metódos responsáveis por modificar a porta
+  desselecionar() {
+    const selecionada = false;
+    return new PortaModel(
+      this.numero,
+      this.temPresente,
+      selecionada,
+      this.aberta
+    );
+  }
+
+  alternarSelecao() {
+    const selecionada = !this.selecionada;
+    return new PortaModel(
+      this.numero,
+      this.temPresente,
+      selecionada,
+      this.aberta
+    );
+  }
+  abrir() {
+    const aberta = true;
+    return new PortaModel(
+      this.numero,
+      this.temPresente,
+      this.selecionada,
+      aberta
+    );
+  }
+}
