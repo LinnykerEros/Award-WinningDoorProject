cb6adf89a4bcef2d64656b1f3a270ec8187d63e3
Eros Linnyker <linnykereros@gmail.com>
2022-07-07 14:51:55 -0300

    Validando formulário

--- src/pages/jogo/[portas]/[temPresente].tsx
+++ src/pages/jogo/[portas]/[temPresente].tsx
@@ -7,7 +7,16 @@ import Link from "../../../../node_modules/next/link";
 import styles from "../../../styles/Jogo.module.css";
 export default function Jogo() {
   const router = useRouter();
+  const [valido, setValido] = useState(false);
   const [portas, setPortas] = useState([]);
+
+  useEffect(() => {
+    const portas = +router.query.portas;
+    const temPresente = +router.query.temPresente;
+    const qtdeportasValidas = portas >= 3 && portas <= 100;
+    const temPresenteValido = temPresente >= 1 && temPresente <= portas;
+    setValido(qtdeportasValidas && temPresenteValido);
+  }, [portas]);
 
   useEffect(() => {
     const portas = +router.query.portas;
@@ -16,17 +25,20 @@ export default function Jogo() {
   }, [router?.query]);
 
   function renderizarPortas() {
-    return portas.map((porta) => {
-      return (
-        <Porta
-          key={porta.numero}
-          value={porta}
-          onChange={(novaPorta) =>
-            setPortas(atualizarPortas(portas, novaPorta))
-          }
-        />
-      );
-    });
+    return (
+      valido &&
+      portas.map((porta) => {
+        return (
+          <Porta
+            key={porta.numero}
+            value={porta}
+            onChange={(novaPorta) =>
+              setPortas(atualizarPortas(portas, novaPorta))
+            }
+          />
+        );
+      })
+    );
   }
   return (
     <div id={styles.jogo}>
